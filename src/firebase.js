07b7a5548f6b3d3bc32f9c0724d7a0453292c54b
Alec Aldrine Lakra <lakraalec123@gmail.com>
2023-01-08 22:58:26 +0530

    chore(firebase): Remove unnecessary code comments

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCJWal6BZtn62QqEcmYpQ90wogJKfphUs0",
-//   authDomain: "algotest-b9347.firebaseapp.com",
-//   projectId: "algotest-b9347",
-//   storageBucket: "algotest-b9347.appspot.com",
-//   messagingSenderId: "218651015292",
-//   appId: "1:218651015292:web:48a6b764157fb0ecaeb1f9"
-// };
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
